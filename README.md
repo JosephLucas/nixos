9adef23d44175cc726d2b61cc3e394006340052d
Joseph Lucas <lucas.jojo@gmail.com>
2019-03-07 12:14:57 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# nixos
+# [NixOS](https://nixos.org/) installation on a Dell XPS 15 9570
+ 
+On windows 10 :
+* Update the EFI/BIOS firmware to the latest version
+* Free space and defrag disk (use PerfectDisk or UltraDefrag). Perform a "bootime defrag" otherwise some system files are locked. Be aware that a NTFS file system has metadata stuck right in the middle of the partition and it is not easy to move them.
+* Due to stuck metadata, reduce the size of the window partition to 50%, this makes room for the NIXOS partition.
+* Create a live USB from a downloaded NixOS iso. Label it NIXOS_ISO (this was important otherwise i got a 
+  "squashfs error... unable to read id index table.").
+
+On hardware config menu (F2 while booting):
+* deactivate RAID and activate AHCI instead. Otherwise you won't be able to mount the device before installation.
+
+On Bootmenu (F12 while booting)
+* try to switch off secure boot to start the live usb, if it does not work try legacy mode
+
+On the live NIXOS:
+* `mkfs.ext4 /dev/xxx`, `e2label /dev/xxx NIXOS`
+* install using configuration.nix and hardware-configuration.nix
+
+On NIXOS:
+* the X server was not working until I updated the kernel (pkgs.linuxPackages_latest). `nomodeset` kernelParam might also help.
+
+## Main differences with the [install of arch linux on an ASUS zenbook pro UX32VD](https://github.com/JosephLucas/archlinux_installation): 
+* systemd-boot instead of rEFInd
+* i3 as window manager
+* icon-theme "rodent" default in xfce instead of gnome-humanity
+* oh-my-zsh instead of grml
+
+## Common commands
+
+Try some packages in the user space
+```bash
+nix-env -iA package
+```
+
+###Free some space
+```bash
+nix-collect-garbage --delete-older-than 3d
+```
+Get all packages installed in the user space
+```bash
+nix-env -q
+```
+Uninstall a package
+```bash
+nix-env -e package
+```
+
+```bash
+ranger --copy-config=all
+```
+
+## TIPS
+"Unlock" the panel of xfce if you want to move it. This can be done in the preference of the panel.
+
+## TODO
+[solve annoying prompt for nextcloud client](https://github.com/NixOS/nixpkgs/issues/38266)
+
+install missing [antidote](https://antidote.info/fr)
---
+++ configuration.nix
@@ -0,0 +1,351 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan + custom swapfile.
+      ./hardware-configuration.nix
+
+      # FIXME : use home-manager if the community decides to integrate it
+      # Module for the management of user dotfiles
+      # https://rycee.gitlab.io/home-manager/index.html
+      # <home-manager/nixos>
+    ];
+  
+  swapDevices = [
+   # cat /proc/meminfo -> MemTotal = 16061732 kB (approx 15318 MiB or 16Gb)
+   # Creates a swapfile
+   { device = "/swapfile"; size = 15318;} # MiB
+   # The swapfile will mainly be used for hibernation, it can be removed if space is needed.    
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    
+    kernelParams = [ 
+       # many parameters are from : https://github.com/JackHack96/dell-xps-9570-ubuntu-respin#manual-respin-procedure
+       "acpi_osi=Linux" 
+       "acpi_rev_override=1" 
+       "pcie_aspm=force" # force Active State Power Management (ASPM) even on devices that claim not to support it
+       "mem_sleep_default=deep"
+
+       # Newest kernels have moved the video mode setting into the kernel. So all the programming of the hardware specific clock rates and registers on the video card happen in the kernel rather than in the X driver when the X server starts. This makes it possible to have high resolution nice looking splash (boot) screens and flicker free transitions from boot splash to login screen. Unfortunately, on some cards this doesnt work properly and you end up with a black screen. Adding the nomodeset parameter instructs the kernel to not load video drivers and use BIOS modes instead until X is loaded.
+       # "nomodeset" # disable kernel modesetting (kms) that sets display resolution and depth in the kernel space
+
+       # In scenarios where you are getting unexplained system freeze scenarios, NMI watchdog interrupt handler will simply kill whatever process happens to be freezing the CPU at the moment. This way, your CPU gets freed up AND you get a detailed stack trace of why your CPU got frozen up in the first place.
+       "nmi_watchhog=1"
+    ];
+
+    initrd = {
+      kernelModules = [
+        # "i915"
+        # "dell-smm-hwmon"
+        "scsi_mod"
+        "drm"
+      ];
+    };
+    
+    # scsi_mod.use_blk_mq=1 # optimises the scheduling of IO on disks using multi-cores
+    # drm.vlblankoffdelay=1 # https://wiki.ubuntu.com/Kernel/PowerManagement/PowerSavingTweaks
+    extraModprobeConfig = ''
+      options scsi_mod use_blk_mq=1
+      options drm vlblankoffdelay=1
+    '';
+    # practical module option when the intel chip is used since initrd
+    # options i915 enable_fbc=1 enable_guc_loading=1 enable_guc_submission=1 disable_power_well=0
+
+    # FIXME: use a kernel module for fans ?
+    # https://github.com/torvalds/linux/blob/master/drivers/hwmon/dell-smm-hwmon.c
+    # options dell-smm-hwmon restricted=0 force=1
+    
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    blacklistedKernelModules = [ "nouveau" ]; # blacklist the opensource nvidia driver that does not work well and might conflict with proprietary nvidia driver
+  };
+
+  virtualisation.docker.enable = true; # dont forget to add the user in the docker group
+
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # to get available wifi : "iwlist <interface> scan"
+    # the interface is found using "ip link show"
+
+    # FIXME wpa_supplicant ?
+
+    firewall.enable = true; # it''s true by default anyway. It is a "statefull firewall".
+    # Open ports in the firewall. 
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
+
+  fonts.fonts = with pkgs; [ 
+    corefonts # Micrsoft free fonts
+    inconsolata  # monospaced
+    nerdfonts # popular 'iconic fonts' 
+    iosevka
+  ];
+  fonts.fontconfig.defaultFonts.monospace = [ "Inconsolata" ];
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris"; # services.localtime might give the same info any-way, but needs internet
+
+  # e.g. steam package has a non-free licence
+  nixpkgs.config.allowUnfree = true;
+
+  # taken from https://github.com/paolobueno/nixos-config/blob/master/configuration.nix
+  programs = {
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      syntaxHighlighting = {
+        enable = true;
+        highlighters = [ "main" "brackets" "pattern" ];
+      };
+      ohMyZsh.enable = true;
+      ohMyZsh.plugins = [ "git" ];
+      ohMyZsh.theme = "pure";
+    };
+    adb.enable = true; # android debuger, for smartphone
+    mosh.enable = true; # mobile shell, pour remplacer ssh+screen dans certains cas
+    ssh.startAgent = true;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # Or go to https://nixos.org/nixos/packages.html#
+  environment.systemPackages = with pkgs; [
+    # xfce4 goodies
+    xfce.xfce4-panel
+    xfce.xfce4-clipman-plugin
+    xfce.xfce4-datetime-plugin
+    xfce.xfce4-pulseaudio-plugin
+    xfce.xfce4-namebar-plugin
+    xfce.xfce4-namebar-plugin
+    xfce.xfce4-whiskermenu-plugin
+    xfce.xfce4-xkb-plugin
+    xfce.xfce4-windowck-plugin
+
+    xfce.thunar-archive-plugin # thunar extension for compressed/archives
+    xfce.thunar-volman # thunar extension for removable disks
+
+    # basic command line utilities
+    feh # for wallpaper on i3
+    file
+    gdb
+    killall
+    zip
+    unzip
+    lsof
+    pciutils # for lspci
+    git
+    wget
+    kdiff3
+    pandoc # universal conversion conversion of written file types
+    xorg.xkill
+    vim
+    w3m # a minimalist cmd line web browser with image previews
+    htop
+    powertop # monitor energy consumption
+    gnupg
+    p7zip
+    rsync
+    ncdu
+    ntfs3g # enable ntfs (FUSE driver with write support)
+    gnome3.file-roller 
+    ncmpcpp # a command line, client interface for mpd the music player daemon
+
+    # GUI apps
+    firefox 
+    thunderbird
+    jetbrains.pycharm-community
+    nextcloud-client
+    evince
+    gimp
+    vlc
+    ranger # minimalist file explorator with vi key bindings and image previews
+    zathura # minimalist document viewer
+    anki # a small app for exercising memorisation on custom questions
+
+    # themes
+    papirus-icon-theme
+
+    # graphic card
+    glxinfo
+    glmark2 # to benchamrk 3D graphics acceleration
+
+    # micro$oft
+    (wine.override { wineBuild = "wineWow"; wineRelease = "staging"; })
+    # To install starcraft 2 I had to : 
+    #   (i)  install firefox within wine (using wintricks it is easier)
+    #   (ii) open a webpage to get the a web certificate needed by the Battle.net agent
+    winetricks
+    # games
+    eduke32
+    steam
+    # NB: steam seems to remember the video card used during its own installation/first start. If the card used to launch the game differs from the card used to install steam, problems may occur.
+    # To totally reset steam : rm -r /home/jlucas/.local/share/Steam .steam
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # Enable sound.
+  sound.enable = true;
+
+  services = {
+    # hardware management
+    fstrim.enable = true; # ssd disk optimisation
+    # udisks2.enable = true; # use dbus to manage storage devices # FIXME usb disk automount
+    # udev.packages = [ pkgs.libmtp.bin ]; # mtp : for android devices
+    tlp.enable = true; # energy/temperature management 
+    thermald.enable = true; # thermal optimisation for intel cpu
+    printing.enable = true; # enable CUPS to print documents
+
+    locate.enable = true; # periodically updatedb
+    localtime.enable = true;
+    redshift = {
+      enable = true;
+      provider = "geoclue2"; # requires services.localtime.enable = true;
+      #latitude = "48.856614";
+      #longitude = "2.3522219";
+    }; # Make sure to not start redshift-gtk, otherwise you will have 2 instances of redhift running, that cause a swicth between standard colors and redshifted colors every 1-2 seconds.
+    
+    openssh = {
+      enable = true;
+      permitRootLogin = "no";
+    };
+
+    # Enable the X11 windowing system.
+    xserver = {
+      enable = true;
+      autorun = true;
+      layout = "us";
+      # Enable touchpad support.
+      libinput.enable = true;
+      
+      # here you can switch the used card at boot
+      videoDrivers = [ "nvidia" ]; 
+      #videoDrivers = [ "intel" ]; 
+      
+      displayManager.lightdm.enable = true;
+      
+      # https://nixos.wiki/wiki/Xfce     + i3wm 
+      desktopManager = {
+        default = "xfce";
+        xterm.enable = false;
+        xfce = {
+          enable = true;
+          noDesktop = true;
+          enableXfwm = false;
+          # https://discourse.nixos.org/t/thunar-doesnt-show-archive-plugin/1499/3
+          thunarPlugins = [ pkgs.xfce.thunar-archive-plugin  pkgs.xfce.thunar-volman ]; # thunar archive plugin does not seem to work
+        };
+      };
+      windowManager.i3.enable = true;
+    };
+
+    # music player daemon
+    mpd = {
+      enable = true;
+      user = "jlucas";
+      group = "users";
+      musicDirectory = "/home/jlucas/Music";
+      dataDir = "/home/jlucas/.mpd";
+      extraConfig = ''
+       # metadata_to_use "artist,album,title,track,name,genre,date,composer,performer,disc,comment"
+        restore_paused "yes"
+        audio_output {
+          type     "pulse"
+          name     "pulse audio"
+        }
+      '';
+      network.listenAddress = "any";
+      startWhenNeeded = true;
+    };
+  };
+
+  hardware = {
+    # Audio
+    pulseaudio = {
+      enable = true;
+      # https://nixos.org/nixpkgs/manual/#sec-steam-play
+      support32Bit = true;
+    };
+     
+    # GPU
+    #https://github.com/NixOS/nixpkgs/issues/24711#issuecomment-401630839
+    # > 10000 FPS with glmark2
+    nvidia = {
+      modesetting.enable = true;
+      optimus_prime = {
+        enable = true;
+        nvidiaBusId = "PCI:1:0:0";
+        intelBusId = "PCI:0:2:0";
+      };
+    };
+    # An alternative solution for switchable graphic cards that works 
+    ### bumblebee
+    #bumblebee = {
+    #  enable = true;
+    #  driver = "nvidia";
+    #  pmMethod = "none"; # this line seems the solution to all my issues !!!
+    #  connectDisplay = true;
+    #};
+    #Dont forget xserver.videoDriver = [ "intel" ]
+    # NB: As explained in https://nixos.wiki/wiki/Nvidia this gives bad performance.
+    # with glmark2: < 400 FPS with the NVIDIA card and intel chip has even better performances
+   
+    # activate 32bit support
+    # opengl.enable = true;
+    opengl.driSupport32Bit = true;
+
+    # CPU
+    cpu.intel.updateMicrocode = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jlucas = {
+    isNormalUser = true;
+    uid = 1000;
+    home = "/home/jlucas";
+    createHome = true;
+    extraGroups = [ "wheel" "audio" "networkmanager" "docker"];
+    initialPassword = "1234";
+    #TODO
+    #openssh.authorizedKeys.keys = # linked with the openssh service
+    #[ "ssh-dss AAAAB3NzaC1kc3MAAACBAPIkGWVEt4..." ];
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
+}
+
+# keep an eye on : 
+#   mopidy # a music daemon like mpd but that can connect to spotify
+#   home-manager # manage dofiles with a declarative nix syntax
+#   primerun :  https://nixos.wiki/wiki/Nvidia and https://github.com/dukzcry/crap/tree/master/primerun  
+# for nvidia/intell config, see :
+#    https://nixos.wiki/wiki/Nvidia (the solution with primerun was not working for me)
+#    https://github.com/NixOS/nixpkgs/issues/24711
+#    https://github.com/NixOS/nixpkgs/files/2707791/configuration.nix.txt
+#    https://github.com/dukzcry/crap/issues/3
